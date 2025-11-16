88d269eb271250e18d1b6f50e16273dfed55352b
HammadMuqaddam <shaikhnomi580@gmail.com>
2025-11-16 15:36:53 +0500

    make changes according to gpt

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const http = require('http');
+const express = require("express");
+const path = require("path");
 
+const app = express();
+
+// serve static files from /public (same as original template)
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.send("Hello from Muqaddam's Elastic Beanstalk app!");
+});
+
+// IMPORTANT: use process.env.PORT (required by EB)
 const port = process.env.PORT || 3000;
-
-http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end("Hello, World!\n");
-}).listen(port);
-
-console.log(`App is running... (port: ${port})`);
+app.listen(port, () => {
+  console.log("Server is running on port", port);
+});
