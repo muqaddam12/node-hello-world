42575604a421ffea835e467bf1f43b04fe8e891a
HammadMuqaddam <shaikhnomi580@gmail.com>
2025-11-21 17:33:10 +0500

    deletion

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
-const port = process.env.PORT || 8080;
+// serve static files from /public (same as original template)
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send("Hello from Elastic Beanstalk + CodePipeline + CodeBuild!");
+  res.send("Hello from Muqaddam's Elastic Beanstalk app!");
 });
 
+// IMPORTANT: use process.env.PORT (required by EB)
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log("Server is running on port", port);
 });
